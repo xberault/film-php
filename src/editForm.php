336c1b146079372d1493c68d6bc6f789e263d1f3
Xavier Berault <xavier.berault@gmail.com>
2020-10-11 14:23:41 +0000

    ajout de l'editionb + deletion des films ( prototypes sans connection bd )
    (cherry picked from commit 7650a9458581e2d394dc604b8d4e1e8874a85440)

---
+++ src/editForm.php
@@ -0,0 +1,18 @@
+<?php
+
+$id=$_GET['id'];
+var_dump($id);
+
+/*
+$query="SELECT * FROM MOVIE WHERE id='$id'";
+
+
+$result = mysqli_query($db,$query) or die( "My query ($query) generated an error: ".mysql_error());
+
+$data=mysqli_fetch_array($result);
+
+*/
+
+
+
+//    mysqli_close($db);
--- src/movie.php
+++ src/movie.php
@@ -1,13 +1,15 @@
 <?php
 
 class Movie{
+    private $id;
     private $name;
     private $date;
     private $author;
     private $gender;
     private $desc;
 
-    public function __construct($name, $date, $author, $gender,$desc="", $img=""){
+    public function __construct($id = 0, $name, $date, $author, $gender,$desc="", $img=""){
+        $this->id = $id;
         $this->name = $name;
         $this->date = $date;
         $this->author = $author;
@@ -17,9 +19,15 @@ class Movie{
 
     public function render(){
         echo <<<EOT
-        <li class="list-inline-item card" style="width:14rem;">
+        <li id="{$this->id}" class="list-inline-item card" style="width:20rem;">
             <img class="card-img-top" src="https://c7.uihere.com/files/881/56/853/film-computer-icons-screenwriter-download-png-movie-icon.jpg" alt="Card image cap">
             <section class="card-body">
+                <a class="btn float-right" href="src/delete.php?id={$this->id}" onclick="return confirm('Etes-vous sûr de vouloir le supprimer ?');">
+                    <i class="far fa-trash-alt"></i>
+                </a>    
+                <a type="button" class="btn float-right" href="src/editForm.php?id={$this->id}" method="get" >
+                    <i class="fas fa-edit"></i>
+                </a>
                 <h5 class="card-title" >{$this->name} </h5>
                 <p class="card-text"> {$this->date}</p>
                 <p class="card-text"> {$this->desc}</p>
