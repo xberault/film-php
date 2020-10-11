eafbcb395859df54ecf5a93690d2c08b42d4f066
Xavier Berault <xavier.berault@gmail.com>
2020-10-11 21:39:53 +0200

    implementation du filtre par date / titre / annee

--- src/editForm.php
+++ src/editForm.php
@@ -1,6 +1,6 @@
 <?php
-
-$id=$_GET['id'];
+$getvalue = $_SESSION['movieList']; // session get
+$id = $_GET['id'];
 var_dump($id);
 
 /*
@@ -12,7 +12,6 @@ $result = mysqli_query($db,$query) or die( "My query ($query) generated an error
 $data=mysqli_fetch_array($result);
 
 */
-
 
 
 //    mysqli_close($db);
