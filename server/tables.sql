65d45b8a7b7d3da63ee7187eb3c397d90bcd9b02
Thomas Jacquet-Francillon <thomas.jacquet-francillon@etu.univ-orleans.fr>
2020-10-14 12:00:02 +0200

    creation table ok

--- server/tables.sql
+++ server/tables.sql
@@ -1,39 +1,39 @@
 
-DROP TABLE REALISE;
-DROP TABLE JOUE;
-DROP TABLE PERSONNE;
-DROP TABLE FILM;
+DROP TABLE IF EXISTS REALISE;
+DROP TABLE IF EXISTS JOUE;
+DROP TABLE IF EXISTS PERSONNE;
+DROP TABLE IF EXISTS FILM;
 
 CREATE TABLE FILM (
-    idFilm NUMBER(10) PRIMARY KEY,
-    titre VARCHAR2(50),
-    synopsis VARCHAR2(255),
-    anneRealisation DATE,
-    genre VARCHAR2(50),
-    posterPath VARCHAR2(200)
+    idFilm INT PRIMARY KEY,
+    titreFilm VARCHAR(50),
+    synopsis VARCHAR(255),
+    dateRealisation DATE,
+    genre VARCHAR(50),
+    posterPath VARCHAR(200)
 );
 
 CREATE TABLE PERSONNE (
-    idPersonnne NUMBER(10) PRIMARY KEY,
-    nom VARCHAR2(50),
-    prenom VARCHAR2(50),
-    sexe VARCHAR2(12),
+    idPersonne INT PRIMARY KEY,
+    nom INT,
+    prenom VARCHAR(50),
+    sexe VARCHAR(12),
     dateNaissance DATE,
-    biographie VARCHAR2(4000)
+    biographie VARCHAR(4000)
 );
 
 CREATE TABLE JOUE (
-    idPersonnne NUMBER(10),
-    idFilm NUMBER(10),
-    CONSTRAINT pkJoue PRIMARY KEY (idPersonnne,idFilm),
-    CONSTRAINT fkIdPersonneJ FOREIGN KEY (idPersonnne) REFERENCES PERSONNE (idPersonnne),
-    CONSTRAINT fkIdFilmJ FOREIGN KEY (idFilm) REFERENCES FILM (idFilm)
+    idPersonne INT,
+    idFilm INT,
+    CONSTRAINT pkJoue PRIMARY KEY (idPersonne,idFilm)
+    -- CONSTRAINT fkIdPersonneJ FOREIGN KEY (idPersonne) REFERENCES PERSONNE (idPersonne),
+    -- CONSTRAINT fkIdFilmJ FOREIGN KEY (idFilm) REFERENCES FILM (idFilm)
 );
 
 CREATE TABLE REALISE (
-    idPersonnne NUMBER(10),
-    idFilm NUMBER(10),
-    CONSTRAINT pkJoue PRIMARY KEY (idPersonnne,idFilm),
-    CONSTRAINT fkIdPersonneR FOREIGN KEY (idPersonnne) REFERENCES PERSONNE (idPersonnne),
-    CONSTRAINT fkIdFilmR FOREIGN KEY (idFilm) REFERENCES FILM (idFilm)
+    idPersonne INT,
+    idFilm INT,
+    CONSTRAINT pkJoue PRIMARY KEY (idPersonne,idFilm)
+    -- CONSTRAINT fkIdPersonneR FOREIGN KEY (idPersonne) REFERENCES PERSONNE (idPersonne),
+    -- CONSTRAINT fkIdFilmR FOREIGN KEY (idFilm) REFERENCES FILM (idFilm)
 );
